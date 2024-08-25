81fe67d3826d0bf64ff304677bebe372bee2b855
Laura Polet <laurapplt@gmail.com>
2024-08-25 18:30:02 +0200

    CSS fix

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import "../assets/styles/layout.scss";
+import NotFoundImage from "../assets/images/404.png";
+import "../assets/styles/NotFound.scss";
 
-const NotFound = () => {
-  return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-    </div>
-  );
-};
+function NotFound() {
+    return (
+        <div className="not-found">
+            <img src={NotFoundImage} alt="404 Not Found" className="404" />
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <a href="/" className="home-link">Retourner à la page d'accueil</a>
+        </div>
+    );
+}
 
 export default NotFound;
